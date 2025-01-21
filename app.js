87b8b964cc7b8155c5339d1a014d17893d670d5b
Raunak Kumar <raunakjsr7016@gmail.com>
2025-01-21 14:44:53 +0530

    Fix:Typing mistake

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ app.all("*",(req,res,next)=>{
     next(new ExpressError(404,"Page not found!"));
 });
 
-app.use((err,rew,res,next)=>{
+app.use((err,req,res,next)=>{
     let{statusCode=500,message="Something went wrong"}=err;
     res.status(statusCode).render("error.ejs",{message})
     //res.status(statusCode).send(message);
